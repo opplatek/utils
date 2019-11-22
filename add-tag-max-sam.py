782575a1bc2c6a18a8cd413072b52f6664f825c0
opplatek <opplatek@gmail.com>
2019-11-21 23:30:07 -0500

    Automatic Backup @ Thu Nov 21 23:30:06 EST 2019

--- add-tag-max-sam.py
+++ add-tag-max-sam.py
@@ -5,6 +5,8 @@
 #
 # Input is sam file, output is sam file (unsorted)
 #
+# It's quite fast but consumes a lot of RAM. For 2.5 GB BAM it needs ~13 GB RAM 
+#
 
 import argparse
 import sys
@@ -19,14 +21,16 @@ parser.add_argument("-o", "--output",
 parser.add_argument("-t", "--tag", type=str, default="ms", 
 				help="Tag name to get the maximum from. Default: ms.")
 parser.add_argument("-n", "--newtag", type=str, default="XP", 
-				help="Tag name to add the count of maximum value of --tag. Default: XP.")
+                help="Tag name to determine 'maximum value hits' (~best hits) from --tag. Can be 0 for no value, 1 for the max value hits, 2 for all the other hits. Default: XP.")
 parser.add_argument("-n2", "--newtag2", type=str, default="XN", 
-                help="Tag name to determine 'the best primary hit' from --tag. Default: XN.")
+				help="Tag name to add the count of maximum value of --tag. Can be 0 for no value, any number for number of max values. Default: XN.")
+
 
 args = parser.parse_args()
 
 tag_scan = args.tag # "ms"
 tag_add = args.newtag # "XP"
+tag_add2 = args.newtag2 # "XN"
 
 t0 = time.time()
 
@@ -35,6 +39,7 @@ if args.input:
     f=open(args.input, "r").read()
 else:
     f=sys.stdin.read()
+
 lines=f.split('\n')
 
 # Get output
@@ -74,15 +79,23 @@ read_singletag = [i for i, x in enumerate([len(x) for x in reads_l]) if x == 1] 
 for x in read_multitag:
     values_x = [int(i) for i in values_l[x]] # Make sure the values are int and not str otherwise max() doesn't work properly
     max_val = values_x.count(max(values_x))
-    for i in reads_l[x]:
-        fout.write(lines[i] + '\t' + tag_add + ':i:' + str(max_val) + '\n')
+    max_val_ind = [i for i, x in enumerate(values_x) if x == max(values_x)] # Get possition of all the max values
+
+    # Get rows by indeces and add first tag, for the rest don't - index to row number (different!) - index is from sublist, row number from total read lines
+    reads_l_max = [reads_l[x][i] for i in max_val_ind] # Get reads with the "best hit"
+    reads_l_rest = [reads_l[x][i] for i in list(range(0, len(values_x))) if i not in max_val_ind] # Get the other read
+
+    for i in reads_l_max:
+        fout.write(lines[i] + '\t' + tag_add + ':i:1' + '\t' + tag_add2 + ':i:' + str(max_val) + '\n') # Add "best hit" tag
+    for i in reads_l_rest:
+        fout.write(lines[i] + '\t' + tag_add + ':i:2' + '\t' + tag_add2 + ':i:' + str(max_val) + '\n') # Add "best hit" tag
 
 flat_list_single = [item for sublist in [reads_l[i] for i in read_singletag] for item in sublist] # Unlist the stupid list https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-list-of-lists
 
 for i in flat_list_single:
-	fout.write(lines[i] + '\t' + tag_add + ':i:1' + '\n')
+	fout.write(lines[i] + '\t' + tag_add + ':i:1' + '\t' + tag_add2 + ':i:1' + '\n') # Add "best hit" tag
 for i in read_notag:
-	fout.write(lines[i] + '\t' + tag_add + ':i:0' + '\n')
+	fout.write(lines[i] + '\t' + tag_add + ':i:0' + '\t' + tag_add2 + ':i:0' + '\n') # Add "no value" tag
 
 if args.output:
     fout.close()
