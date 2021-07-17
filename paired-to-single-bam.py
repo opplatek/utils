9ca307dfbf4ba63f7bfdff331f1e1b83e1f00e6c
opplatek <opplatek@gmail.com>
2021-07-16 23:30:16 -0400

    Automatic Backup @ Fri Jul 16 23:30:10 EDT 2021

--- paired-to-single-bam.py
+++ paired-to-single-bam.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Take paired-end BAM, get only first|second in a pair read and change SAM flags so it looks like it's single-end
+# Take paired-end BAM, get only first|second in a pair (not both!) read and change SAM flags so it looks like it's single-end
 #
 # https://bioinformatics.stackexchange.com/questions/9228/convert-paired-end-bam-into-a-single-end-bam-and-keep-all-the-reads
 #
@@ -19,8 +19,6 @@ mate_reverse =   32
 first_in_pair =  64
 second_in_pair = 128
 
-print(sys.argv)
-
 pair = sys.argv[1] # first|second in a pair to preserve
 ibam = sys.argv[2] # Input bam
 obam = sys.argv[3] # Output bam
@@ -36,11 +34,13 @@ else:
 bam_in = pysam.AlignmentFile(ibam, "rb")
 bam_out = pysam.AlignmentFile(obam, "wb", template=bam_in)
 for line in bam_in:
-    if line.flag & keep:
+#    if line.flag & second_in_pair: # This was in the original script but I am not sure what was the reason for this
 #        line.pos += line.template_length
-        continue
-#    line.next_reference_id = 0
-#    line.next_reference_start = 0
-    line.flag &= ~(paired + proper_pair + mate_unmapped + mate_reverse + first_in_pair + second_in_pair)
+    if line.flag & keep:
+#        line.next_reference_id = 0 # Set ref to "*" (=no ref)
+        line.next_reference_name = "*" # Set ref to "*" (=no ref)
+        line.next_reference_start = -1 # Set mate start pos to unset (=0)
+        line.template_length = -1 # Set template length to unset (=0)
+        line.flag &= ~(paired + proper_pair + mate_unmapped + mate_reverse + first_in_pair + second_in_pair) # Remove all these flags if present
 
-    bam_out.write(line)
+        bam_out.write(line)
