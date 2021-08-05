3141e529da05764db2ae3eb76f26848420c01d26
opplatek <opplatek@gmail.com>
2021-08-04 23:30:18 -0400

    Automatic Backup @ Wed Aug  4 23:30:14 EDT 2021

--- subsample_sam.py
+++ subsample_sam.py
@@ -1,17 +1,1 @@
-#!/usr/bin/env python3
-#
-# Subsample SAM file by list of reads
-# https://bioinformatics.stackexchange.com/questions/3380/how-to-subset-a-bam-by-a-list-of-qnames (wjv)
-#
-# Run as: samtools view input.bam | python3 subsample_sam.py qnames.txt
-#
-
-import sys
-
-with open(sys.argv[1], 'r') as indexfile:
-    ids = set(l.rstrip('\r\n') for l in indexfile)
-
-for line in sys.stdin:
-    qname, _ = line.split('\t', 1)
-    if qname in ids:
-        sys.stdout.write(line)
+sam-subsample.py
