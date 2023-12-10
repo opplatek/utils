af5bf230412e94a2fa7fee2ca39b987c45177b12
opplatek <opplatek@gmail.com>
2023-12-09 23:30:01 -0500

    Automatic Backup @ Sat Dec  9 23:30:01 EST 2023

--- revigo_api.py
+++ revigo_api.py
@@ -1,102 +1,124 @@
 #!/usr/bin/python3
 #
-# Python script for programtic access to Revigo. Run it with (last output file name is optional):
-# python3 revigo.py example.csv 9606
-# http://revigo.irb.hr/FAQ.aspx -> How do I integrate Revigo with my service or a programming language? -> The advanced job submitting method
+# Python script for programtic access to Revigo. Input is a list of GO terms, optionally with pvalue in the second column
+#   separated by a comma. No header.
+#
+# API described at http://revigo.irb.hr/FAQ#q01 -> How do I integrate Revigo with my service or a programming language? ->
+#    The advanced job submitting method -> (scroll down) Programming examples
 #
 # Make revigo python virtual env.
 #python3 -m venv revigo
 #source revigo/bin/activate
 #pip install --upgrade pip
 #pip install requests
-#
-#R # install required R packages
+# Install required R packages
+#R
 #install.packacges("treemap")
 #
 
 import requests
 import time
-import sys
 import os
+import argparse
 
-# Read enrichments file
-userData = open(sys.argv[1],'r').read()
-
-taxon = sys.argv[2]
-#taxon = '9606' # human
-#taxon = '10090' # mouse
-#taxon = '4932' # yeast
+argParser = argparse.ArgumentParser()
+argParser.add_argument(
+    "-i",
+    "--input",
+    help="Input GO summary file. No header. Two tab separated columns - first column is GO ID, second is confidence value (for example p-value)",
+    required=True,
+)
+argParser.add_argument(
+    "-t",
+    "--taxon",
+    help="NCBI taxon ID https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi. Default: 0 (no taxon)",
+    default=0,
+    required=False,
+)
+argParser.add_argument(
+    "-o",
+    "--output",
+    help="Output file prefix. The script will generate several files with this prefix. Default: revigo.txt",
+    default="revigo.txt",
+    required=False,
+)
+args = argParser.parse_args()
+print("args=%s" % args)
 
 # Submit job to Revigo
-payload = {'cutoff':'0.7', 'valueType':'pvalue', 'speciesTaxon':taxon, 'measure':'SIMREL', 'goList':userData}
-r = requests.post("http://revigo.irb.hr/StartJob.aspx", data=payload)
+with open(args.input, "r") as f:
+    userData = f.read()
+    payload = {
+        "cutoff": "0.7",
+        "valueType": "pvalue",
+        "speciesTaxon": args.taxon,
+        "measure": "SIMREL",
+        "removeObsolete": "true",
+        "goList": userData,
+    }
+r = requests.post("http://revigo.irb.hr/StartJob", data=payload)
 
-jobid = r.json()['jobid']
+jobid = r.json()["jobid"]
 
 # Check job status
 running = 1
-while (running!=0):
-    r = requests.post("http://revigo.irb.hr/QueryJobStatus.aspx", data={'jobid':jobid})
-    running = r.json()['running']
+while running != 0:
+    type = "jstatus"
+    r = requests.get(f"http://revigo.irb.hr/QueryJob?jobid={jobid}&type={type}")
+    running = r.json()["running"]
     time.sleep(1)
 
-print(jobid)
-
 # Fetch results
-#    "jobid" - Job ID that you collected in the first step.
-#    "namespace" - [1, 2, 3] The namespace for which you are collecting results. 1 represents BIOLOGICAL_PROCESS, 2 represents CELLULAR_COMPONENT, 3 represents MOLECULAR_FUNCTION.
-#    "type" - [csvtable, rtable, xgmml, csvtree, rtree] The type of the output that you require. CSVTable gets the resulting table with Scatterplot data. RTable gets the R script for Scatterplot. Xgmml gets the xgmml for Cytoscape. CSVTree gets the Tree Map data in a comma separated values format. RTree gets the R script for Tree Map.
+# "namespace" - [1, 2, 3] The namespace for which you are collecting results.
+#   1 represents 'Biological process', 2 represents 'Cellular component', 3 represents 'Molecular function'.
+namespace_explain = {
+    "1": "biological_process",
+    "2": "cellular_component",
+    "3": "molecular_function",
+}
+# "type" - [jStatus, jInfo, jTable, Table, jScatterplot, Scatterplot, RScatterplot, jScatterplot3D, Scatterplot3D,
+#   jCytoscape, Xgmml, jTreeMap, TreeMap, RTreeMap, jClouds, SimMat] The type of the output that you require.
+type_suffix = {
+    "Table": "tsv",
+    "Scatterplot": "tsv",
+    "RScatterplot": "R",
+    "Scatterplot3D": "tsv",
+    "Xgmml": "xgmml",
+    "TreeMap": "tsv",
+    "RTreeMap": "R",
+    "jClouds": "json",
+}
+for namespace in namespace_explain.keys():
+    namespace_translate = namespace_explain[namespace]
+    for type in type_suffix.keys():
+        suffix = type_suffix[type]
+        r = requests.get(
+            f"http://revigo.irb.hr/QueryJob?jobid={jobid}&namespace={namespace}&type={type}"
+        )
+        ofile_res = (
+            f"{os.path.splitext(args.output)[0]}-{namespace_translate}-{type}.{suffix}"
+        )
+        with open(ofile_res, "w") as f:
+            f.write(r.text.replace("\r\n", "\n"))
 
-namespace = [1, 2, 3]
-for i in namespace:
-    if i == 1:
-        name = "bp"
-    elif i == 2:
-        name = "cc"
-    elif i == 3:
-        name = "mf"
+    # Plot R scripts
+    scatterplot = f"Revigo-{namespace_translate}-RScatterplot"
+    with open(f"{scatterplot}.R", "a") as f:
+        f.write(f'ggsave("{scatterplot}.pdf", width=10)')
 
-    print(i)
-    print(name)
+    with open(f"{scatterplot}.R", "r") as f:
+        if "error: The namespace" not in f.readline():
+            os.system(f"R CMD BATCH {scatterplot}.R")
+            if os.path.exists(
+                "Rplots.pdf"
+            ):  # in case R failed (possibly no ggplot2 library)
+                os.remove("Rplots.pdf")
 
-    # CSV
-    r = requests.post("http://revigo.irb.hr/ExportJob.aspx", data={'jobid':jobid, 'namespace':i, 'type':'csvtable'})
-    with open('Revigo-' + name + '.csv', 'w') as f:
-        f.write(r.text)
-
-    # scatterplot R
-    r = requests.post("http://revigo.irb.hr/ExportJob.aspx", data={'jobid':jobid, 'namespace':i, 'type':'rtable'})
-    with open('RevigoScatterplot-' + name + '.R', 'w') as f:
-        f.write(r.text)
-    with open('RevigoScatterplot-' + name + '.R', 'a') as f:
-        # Downloaded scatter script doesn't save PDF, so add this line
-        f.write('ggsave("' + 'RevigoScatterplot-' + name + '.pdf"' + ', width=10)')
-
-    # cytoscape
-    r = requests.post("http://revigo.irb.hr/ExportJob.aspx", data={'jobid':jobid, 'namespace':i, 'type':'xgmml'})
-    with open('RevigoCytoscape-' + name + '.xgmml', 'w') as f:
-        f.write(r.text)
-
-    # tree
-    r = requests.post("http://revigo.irb.hr/ExportJob.aspx", data={'jobid':jobid, 'namespace':i, 'type':'csvtree'})
-    with open('RevigoTree-' + name + '.csv', 'w') as f:
-        f.write(r.text)
-
-    # tree R
-    r = requests.post("http://revigo.irb.hr/ExportJob.aspx", data={'jobid':jobid, 'namespace':i, 'type':'rtree'})
-    # Write results to a file - if file name is not provided the default is result.csv
-    with open('RevigoTree-' + name + '.R', 'w') as f:
-        f.write(r.text)
-
-    # Run R scripts and make figures
-    with open('RevigoScatterplot-' + name + '.R') as f:
-        if "Error: The namespace" not in f.readline():
-            os.system('R CMD BATCH ' + 'RevigoScatterplot-' + name + '.R')
-            if os.path.exists('Rplots.pdf'): # in case R failed (possibly no ggplot2 library
-                os.remove('Rplots.pdf') # remove default figure
-
-    with open('RevigoTree-' + name + '.R') as f:
-        if "Error: The namespace" not in f.readline():
-            os.system('R CMD BATCH ' + 'RevigoTree-' + name + '.R')
-            if os.path.exists('revigo_treemap.pdf'): # in case R failed (possibly no treemap library
-                os.rename('revigo_treemap.pdf', 'RevigoTree-' + name + '.pdf')
+    treemap = f"Revigo-{namespace_translate}-RTreeMap"
+    with open(f"{treemap}.R", "r") as f:
+        if "error: The namespace" not in f.readline():
+            os.system(f"R CMD BATCH {treemap}.R")
+            if os.path.exists(
+                "revigo_treemap.pdf"
+            ):  # in case R failed (possibly no treemap library)
+                os.rename("revigo_treemap.pdf", f"{treemap}.pdf")
